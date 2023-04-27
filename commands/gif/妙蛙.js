512df315c18c1b9b191cfd355498a8feb1fa0635
ChaosConcerto <edelweiss0727@gmail.com>
2023-04-27 20:11:23 +0800

    [妙蛙OK]

--- commands/gif/妙蛙.js
+++ commands/gif/妙蛙.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	cooldown: 10,
+	cooldown: 5,
 	data: new SlashCommandBuilder()
-		.setName('!妙蛙')
-		.setDescription('妙蛙種子!'),
+		.setName('miaowa')
+		.setDescription('miao_wa'),
 	async execute(interaction) {
 		await interaction.reply('https://cdn.discordapp.com/attachments/1020572202917167126/1098587614119993404/3.gif');
 	},
