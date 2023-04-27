e0a88f8fb1df0597f2eaa7aca764fbd4ec3555ef
ChaosConcerto <edelweiss0727@gmail.com>
2023-04-27 20:33:07 +0800

    [歐洲改歐]

--- commands/gif/歐.js
+++ commands/gif/歐.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
-		.setName('歐洲')
-		.setDescription('歐洲'),
+		.setName('歐')
+		.setDescription('歐'),
 	async execute(interaction) {
 		await interaction.reply('https://media.discordapp.net/attachments/721327813298356304/1034788320741441586/71BF2559-F0D2-4CDE-ADBC-3813BB4A6114.gif');
 	},
