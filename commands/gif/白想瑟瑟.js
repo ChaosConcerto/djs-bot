6d57acb91b290d92aa75cbe82d3a98686080479e
ChaosConcerto <edelweiss0727@gmail.com>
2023-06-18 13:25:38 +0800

    [白想瑟瑟]

---
+++ commands/gif/白想瑟瑟.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('白想瑟瑟')
+		.setDescription('白想瑟瑟'),
+	async execute(interaction) {
+		await interaction.reply('https://media.discordapp.net/attachments/1020572202917167126/1102213718802190336/v2-53461ae9bddedec2d46ce7a4d030b457_b.png');
+	},
+};
